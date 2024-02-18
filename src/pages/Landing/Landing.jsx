0558fced704275460de5297c99d99f56cdac7bf5
BertoPM <bertopolo@gmail.com>
2024-02-18 18:10:33 +0100

    Index

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,17 +2,77 @@ import React from "react";
 import "./styles/landingStyles.css"
 
 export const Landing = () => {
-  return (
-    <>
-        <h1 id="presentation">
-            Soy&nbsp;
-            <span className="front"></span>
-            <span className="back"></span>
-            <span className="creativo"></span>
-            <span className="dinamico"></span>
-            <span className="polo"></span>
-        </h1>
+return (
+	<>
 
-    </>
-  )
+		<section className="hero">
+			<h1 id="caret">
+				Soy&nbsp;
+					<span className="front"></span>
+					<span className="back"></span>
+					<span className="creativo"></span>
+					<span className="dinamico"></span>
+					<span className="polo"></span>
+			</h1>
+		</section>
+		<section id="about">
+
+			<div className="scroll-banner">
+				<div className="text">
+					<span className="scrolling-element">Bienvedido a mi portfolio</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">Un placer conocerte</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">Gracias por venir</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">Disfruta la visita</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">¿Hablamos?</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+				</div>
+				<div className="text">
+				<span className="scrolling-element">Bienvedido a mi portfolio</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">Un placer conocerte</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">Gracias por venir</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>
+					<span className="scrolling-element">Disfruta la visita</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>					
+					<span className="scrolling-element">¿Hablamos?</span>
+					<span className="flower">
+						<img src="../../../public/flower40.svg" alt="" />
+					</span>					
+				</div>
+			</div>
+			<div className="main">
+				<h1>¡Hola!</h1>
+				<p>
+					Antes que nada, agradecerte que te hayas tomado el tiempo de pasar por
+				</p>
+
+			</div>
+
+
+		</section>
+	</>
+)
 }
